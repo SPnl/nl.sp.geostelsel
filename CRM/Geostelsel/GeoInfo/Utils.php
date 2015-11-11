4541743c8ff0c227e947b11673d5ebafc18ccd71
Jaap Jansma <jaap@edeveloper.nl>
2015-11-11 14:22:54 +0100

    update finding of regio and provincie

--- CRM/Geostelsel/GeoInfo/Utils.php
+++ CRM/Geostelsel/GeoInfo/Utils.php
@@ -8,10 +8,16 @@ class CRM_Geostelsel_GeoInfo_Utils {
       $params['return'] = 'contact_id_b';
       $params['relationship_type_id'] = $config->getRegioRelationshipTypeId();
       $params['contact_id_a'] = $afdelings_contact_id;
-      $params['is_active'] = 1;
-      $contact_id = civicrm_api3('Relationship', 'getvalue', $params);
+      $params['status_id'] = CRM_Contact_BAO_Relationship::CURRENT;
+      
+      $relationships = CRM_Contact_BAO_Relationship::getRelationship($afdelings_contact_id, CRM_Contact_BAO_Relationship::CURRENT, 0, 0, 0, NULL, NULL, false, $params);
+      if (count($relationships)) {
+        $relationship = reset($relationships);
+        $contact_id = $relationship['contact_id_b'];
+      }
       return $contact_id;
     } catch (Exception $ex) {
+      throw $ex;
       //do nothing
     }
     return false;
@@ -23,8 +29,14 @@ class CRM_Geostelsel_GeoInfo_Utils {
       $params['return'] = 'contact_id_b';
       $params['relationship_type_id'] = $config->getProvincieRelationshipTypeId();
       $params['contact_id_a'] = $regio_contact_id;
-      $params['is_active'] = 1;
-      $contact_id = civicrm_api3('Relationship', 'getvalue', $params);
+      $params['status_id'] = CRM_Contact_BAO_Relationship::CURRENT;
+
+      $relationships = CRM_Contact_BAO_Relationship::getRelationship($regio_contact_id, CRM_Contact_BAO_Relationship::CURRENT, 0, 0, 0, NULL, NULL, false, $params);
+      if (count($relationships)) {
+        $relationship = reset($relationships);
+        $contact_id = $relationship['contact_id_b'];
+      }
+
       return $contact_id;
     } catch (Exception $ex) {
       //do nothing
