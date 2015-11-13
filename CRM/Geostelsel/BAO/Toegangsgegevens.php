464b4daec0bd0fc3532b8c10ea6d1aea2c60786c
Jaap Jansma <jaap@edeveloper.nl>
2015-11-13 12:20:20 +0100

    group and afdeling where rebuild

--- CRM/Geostelsel/BAO/Toegangsgegevens.php
+++ CRM/Geostelsel/BAO/Toegangsgegevens.php
@@ -39,4 +39,58 @@ class CRM_Geostelsel_BAO_Toegangsgegevens {
     return $values;
   }
 
+  public static function getWhereForGroup($group_id, &$tables, &$whereTables) {
+    $dao = CRM_Core_DAO::executeQuery("SELECT * FROM civicrm_group where id = ".$group_id);
+    $clauses = array();
+    while ($dao->fetch()) {
+      $selects = unserialize($dao->select_tables);
+      foreach($selects as $table => $join) {
+        $tables[$table] = $join;
+      }
+      $wheres = unserialize($dao->where_tables);
+      foreach($wheres as $table => $join) {
+        $whereTables[$table] = $join;
+      }
+      $clauses[] = $dao->where_clause;
+    }
+
+    if (!empty($clauses)) {
+      return " ( ".implode(" AND ", $clauses)." ) ";
+    }
+    return "";
+  }
+
+  public static function getWhereForAfddeling($afdeling_id, &$tables, &$whereTables) {
+    $config = CRM_Geostelsel_Config::singleton();
+    $table = $config->getGeostelselCustomGroup('table_name');
+    $afdeling = $config->getAfdelingsField('column_name');
+    $regio = $config->getRegioField('column_name');
+    $provincie = $config->getProvincieField('column_name');
+    $tables['geostelsel'] = $whereTables['geostelsel'] = "LEFT JOIN {$table} geostelsel ON contact_a.id = geostelsel.entity_id";
+
+    //add active membership
+    $membership_type = CRM_Geostelsel_Config_MembershipTypes::singleton();
+    $membership_table = 'civicrm_membership';
+    $tables['membership_access'] = $whereTables['membership_access'] = "LEFT JOIN {$membership_table} membership_access ON contact_a.id = membership_access.contact_id";
+    $mtype_ids = implode(", ", $membership_type->getMembershipTypeIds());
+    $mstatus_ids = implode(", ", $membership_type->getStatusIds());
+
+
+    return "
+        (
+          geostelsel.`{$afdeling}` = {$afdeling_id}
+          OR geostelsel.`{$regio}` = {$afdeling_id}
+          OR geostelsel.`{$provincie}` = {$afdeling_id}
+        )
+        AND
+        (
+          membership_access.membership_type_id IN ({$mtype_ids})
+          AND (
+            membership_access.status_id IN ({$mstatus_ids})
+            OR
+            (membership_access.status_id = '{$membership_type->getDeceasedStatusId()}' AND (membership_access.end_date >= NOW() - INTERVAL 3 MONTH))
+          )
+        )";
+  }
+
 }
