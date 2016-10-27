4cbc67848817fa925a823f34b403ace86f851f0a
Jaap Jansma <jaap@edeveloper.nl>
2016-10-27 13:18:08 +0200

    Update performance improvement

--- CRM/Geostelsel/AclGenerator.php
+++ CRM/Geostelsel/AclGenerator.php
@@ -8,9 +8,24 @@ class CRM_Geostelsel_AclGenerator {
       return;
     }
 
+    /**
+     * Add contacts a user is allowed to see to the session.
+     * So that the ACL queries performans fatser by only doing an 'IN" on contact_id
+     * rather than complicated joins and wheres.
+     *
+     * We store all ids of contacts a user is allowed to see in the session.      *
+     * If the data in the session is more than 8 hours old rebuild the data.
+     *
+     */
     $session = CRM_Core_Session::singleton();
     $contacts = $session->get('acl_contacts', 'nl.sp.accesscontrol');
-    if (!$contacts) {
+    $timestamp = $session->get('acl_contacts_timestamp', 'nl.sp.accesscontrol');
+    $maxTimestamp = 8*60*60; //8 hours
+    $reset = false;
+    if ($timestamp && (time()-$timestamp) > $maxTimestamp) {
+      $reset = true;
+    }
+    if (!$contacts || $reset) {
       $aclCacheTables = array();
       $aclWhereTables = array();
       $aclCacheWhere = "";
@@ -28,6 +43,7 @@ class CRM_Geostelsel_AclGenerator {
         $contacts[] = $dao->contact_id;
       }
       $session->set('acl_contacts', serialize($contacts), 'nl.sp.accesscontrol');
+      $session->set('acl_contacts_timestamp', time(), 'nl.sp.accesscontrol');
     } else {
       $contacts = unserialize($contacts);
     }
