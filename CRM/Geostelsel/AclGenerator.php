c3a66943786b633c34403aabf2d6d2ac039ba3d7
Jaap Jansma <jaap@edeveloper.nl>
2016-10-27 11:26:37 +0200

    first draft of performance issues

--- CRM/Geostelsel/AclGenerator.php
+++ CRM/Geostelsel/AclGenerator.php
@@ -8,7 +8,34 @@ class CRM_Geostelsel_AclGenerator {
       return;
     }
 
-    CRM_Geostelsel_BAO_Toegangsgegevens::generateAclWhere($tables, $whereTables, $contactID, $where);
+    $session = CRM_Core_Session::singleton();
+    $contacts = $session->get('acl_contacts', 'nl.sp.accesscontrol');
+    if (!$contacts) {
+      $aclCacheTables = array();
+      $aclWhereTables = array();
+      $aclCacheWhere = "";
+      CRM_Geostelsel_BAO_Toegangsgegevens::generateAclWhere($aclCacheTables, $aclWhereTables, $contactID, $aclCacheWhere);
+      $aclCacheFrom = CRM_Contact_BAO_Query::fromClause($aclWhereTables);
+
+      $sql = "SELECT contact_a.id as contact_id
+            {$aclCacheFrom}
+            WHERE 1
+            AND {$aclCacheWhere}";
+
+      $dao = CRM_Core_DAO::executeQuery($sql);
+      $contacts = array();
+      while($dao->fetch()) {
+        $contacts[] = $dao->contact_id;
+      }
+      $session->set('acl_contacts', serialize($contacts), 'nl.sp.accesscontrol');
+    } else {
+      $contacts = unserialize($contacts);
+    }
+    if (count($contacts) > 0) {
+      $where .= " OR (contact_a.id IN (" . implode(", ", $contacts)."))";
+    }
+
+
   }
 
 }
