c8d70d3aff8c01547b950dbda2d8d2f3dc790dae
Jaap Jansma <jaap@edeveloper.nl>
2017-01-25 11:46:56 +0100

    improved acl query

--- CRM/Geostelsel/BAO/Toegangsgegevens.php
+++ CRM/Geostelsel/BAO/Toegangsgegevens.php
@@ -167,44 +167,37 @@ class CRM_Geostelsel_BAO_Toegangsgegevens {
     $afdeling = $config->getAfdelingsField('column_name');
     $regio = $config->getRegioField('column_name');
     $provincie = $config->getProvincieField('column_name');
-    $tables['geostelsel'] = $whereTables['geostelsel'] = "LEFT JOIN {$table} geostelsel ON contact_a.id = geostelsel.entity_id";
+    //$tables['geostelsel'] = $whereTables['geostelsel'] = "LEFT JOIN {$table} geostelsel ON contact_a.id = geostelsel.entity_id";
 
     //add active membership
     $membership_type = CRM_Geostelsel_Config_MembershipTypes::singleton();
     $membership_table = 'civicrm_membership';
-    $tables['membership_access'] = $whereTables['membership_access'] = "LEFT JOIN {$membership_table} membership_access ON contact_a.id = membership_access.contact_id";
+    //$tables['membership_access'] = $whereTables['membership_access'] = "LEFT JOIN {$membership_table} membership_access ON contact_a.id = membership_access.contact_id";
     $mtype_ids = implode(", ", $membership_type->getMembershipTypeIds());
     $mstatus_ids = implode(", ", $membership_type->getStatusIds());
 
     $contact_sub_type = CRM_Core_DAO::singleValueQuery("SELECT contact_sub_type from civicrm_contact where id = %1", array(1=>array($afdeling_id, 'Integer')));
     if (stristr($contact_sub_type, CRM_Core_DAO::VALUE_SEPARATOR."SP_Landelijk".CRM_Core_DAO::VALUE_SEPARATOR)) {
       return "
-      (
-        membership_access.membership_type_id IN ({$mtype_ids})
-        AND (
-          membership_access.status_id IN ({$mstatus_ids})
-          OR
-          (membership_access.status_id = '{$membership_type->getDeceasedStatusId()}' AND (membership_access.end_date >= NOW() - INTERVAL 3 MONTH))
-        )
-      ) OR contact_a.id = {$afdeling_id}";
+      contact_a.id IN (
+        SELECT geostelsel.entity_id 
+        FROM {$membership_table} membership_access
+        WHERE membership_access.membership_type_id IN ({$mtype_ids})
+        AND (membership_access.status_id IN ({$mstatus_ids}) OR (membership_access.status_id = '{$membership_type->getDeceasedStatusId()}' AND membership_access.end_date >= NOW() - INTERVAL 3 MONTH))
+      ) 
+      OR contact_a.id = {$afdeling_id}";
     } else {
-      return "
-      (
-        (
-          geostelsel.`{$afdeling}` = {$afdeling_id}
-          OR geostelsel.`{$regio}` = {$afdeling_id}
-          OR geostelsel.`{$provincie}` = {$afdeling_id}
-        )
-        AND
-        (
-          membership_access.membership_type_id IN ({$mtype_ids})
-          AND (
-            membership_access.status_id IN ({$mstatus_ids})
-            OR
-            (membership_access.status_id = '{$membership_type->getDeceasedStatusId()}' AND (membership_access.end_date >= NOW() - INTERVAL 3 MONTH))
-          )
-        )
-      ) OR contact_a.id = {$afdeling_id}";
+      return " 
+      contact_a.id IN (
+        SELECT geostelsel.entity_id 
+        FROM {$table} geostelsel 
+        INNER JOIN {$membership_table} membership_access ON geostelsel.entity_id = membership_access.contact_id
+        WHERE
+        (geostelsel.`{$afdeling}` = {$afdeling_id} OR geostelsel.`{$regio}` = {$afdeling_id} OR geostelsel.`{$provincie}` = {$afdeling_id})
+        AND membership_access.membership_type_id IN ({$mtype_ids}) 
+        AND (membership_access.status_id IN ({$mstatus_ids}) OR (membership_access.status_id = '{$membership_type->getDeceasedStatusId()}' AND membership_access.end_date >= NOW() - INTERVAL 3 MONTH))
+      )
+      OR contact_a.id = {$afdeling_id}";
     }
   }
 
